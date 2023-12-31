e76dff12a61cdb90e802bb57cfe04e15d5f47a97
Shambhavi Rai <112109952+luna1902@users.noreply.github.com>
2023-12-31 19:58:48 +0530

    Das

--- src/app/fpage/page.jsx
+++ src/app/fpage/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
  {
     return(
         <div className=" h-screen flex  bg-cover bg-opacity-75" style={{backgroundImage: "url('/pages.jpg')" }}>
-           
+           <h1 class=" text-4xl font-extrabold"> Dashboard</h1>
             </div>
     )
  }
