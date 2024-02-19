8e78d8159f4b3bcfb57f0dfb2e8bdff2a0c428f9
Shambhavi Rai <112109952+luna1902@users.noreply.github.com>
2024-02-19 18:12:28 +0530

    try making white box visible only with the data

--- src/app/searchBar/page.jsx
+++ src/app/searchBar/page.jsx
@@ -4,6 +4,42 @@
 // import { useState } from "react";
 // import { FaMagnifyingGlass } from "react-icons/fa6";
 
+// function Box()
+// {
+//     return(
+//         <div className=" flex justify-center mr-6 ">
+             
+//            <ul className=" bg-slate-100 h-60 w-96 overflow-auto border-3 border-slate-800" >
+//                 {results.map((result, index) =>
+//                     <div key={index}>
+                    
+//                          <div>
+//                             <strong>Title:</strong> {result.title ? result.title : "N/A"}
+//                         </div>
+//                         <div>
+//                             <strong>Author(s):</strong> {result.author_name ? result.author_name.join(", ") : "N/A"}
+//                         </div>
+//                         <div>
+//                             <strong>Contributor(s):</strong> {result.contributor ? result.contributor : "N/A"}
+//                         </div>
+//                         <div>
+//                             <strong>Ebook Access</strong> {result.ebook_access ?result.ebook_access: "N/A"}
+//                         </div>
+                        
+//                         <div>
+//                             <strong>Currently Reading Count</strong> {result.currently_reading_count ? result.currently_reading_count: "N/A"}
+//                         </div>
+//                         <div>
+//                             <strong>Already Read Count</strong> {result.already_read_count? result.already_read_count : "N/A"}
+//                         </div>
+//                         <div class="border-b border-solid border-black "></div>
+//                     </div>
+//                 )}
+//             </ul>
+            
+//            </div>
+//     )
+// }
 
 
 import React, { useState } from "react";
@@ -18,43 +54,34 @@ export default function Search() {
             const response = await fetch(`http://openlibrary.org/search.json?q=${query}`);
             const data = await response.json();
             setResults(data.docs);
+            //  <Box/>
         } catch (error) {
             console.error('Error fetching search results:', error);
         }
+        
     };
-
+//
     return (
-        <div> 
+        <div > 
            
             <div className=" flex justify-center mt-52">
             <input
                 type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
-                placeholder="Looking for?..."
+                placeholder="Find book details...."
                 className=" border-4 h-14 p-2 w-1/3 border-slate-950 rounded-lg text-black"
             />
-            <button type='submit' className=" flex flex-col text-black" onClick={handleSearch}><FaSearch /></button>
-            {/* <ul>
-                {results.map((result, index) =>
-                    <li key={index}>
-                    
-                         <div>
-                            <strong>Title:</strong> {result.title ? result.title : "N/A"}
-                        </div>
-                        <div>
-                            <strong>Author(s):</strong> {result.author_name ? result.author_name.join(", ") : "N/A"}
-                        </div>
-                    </li>
-                )}
-            </ul> */}
+            <button type='submit' className=" flex flex-col text-black " onClick={handleSearch}><FaSearch className=" mt-5 ml-1 size-5 hover:bg-white hover:rounded-lg " /></button>
+           
             </div>
            
 
-            
-            <ul >
+           <div className=" flex justify-center mr-6 ">
+             
+           <ul className=" bg-slate-100 h-60 w-96 overflow-auto border-3 border-slate-800" >
                 {results.map((result, index) =>
-                    <li key={index}>
+                    <div key={index}>
                     
                          <div>
                             <strong>Title:</strong> {result.title ? result.title : "N/A"}
@@ -62,10 +89,25 @@ export default function Search() {
                         <div>
                             <strong>Author(s):</strong> {result.author_name ? result.author_name.join(", ") : "N/A"}
                         </div>
-                    </li>
+                        <div>
+                            <strong>Contributor(s):</strong> {result.contributor ? result.contributor : "N/A"}
+                        </div>
+                        <div>
+                            <strong>Ebook Access</strong> {result.ebook_access ?result.ebook_access: "N/A"}
+                        </div>
+                        
+                        <div>
+                            <strong>Currently Reading Count</strong> {result.currently_reading_count ? result.currently_reading_count: "N/A"}
+                        </div>
+                        <div>
+                            <strong>Already Read Count</strong> {result.already_read_count? result.already_read_count : "N/A"}
+                        </div>
+                        <div class="border-b border-solid border-black "></div>
+                    </div>
                 )}
             </ul>
             
+           </div>
         </div>
     );
 }
